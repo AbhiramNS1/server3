432a33ccf9496ccc8a03c7c67135c34616abeeb0
Abhiram <abhiramns345@gmail.com>
2022-10-21 16:12:29 +0530

    hello world

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Server } = require('ws')
-
 const socket= new Server({port:4000})
-
 var Clients =[]
 
 socket.on('connection',ws=>{
     Clients.push(ws)
-    ws.on('message',console.log)
+    ws.on('message',(e)=>{
+        console.log(e.toString())
+    })
     ws.on('close',console.log)
     ws.on('error',console.log)
 })
